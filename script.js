f0fb3422c4a101e7a752b1ebbc2c694fc60aefb7
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2020-10-26 17:03:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const menuIcon = document.querySelector('.menu');
+const menuItems = document.querySelector('.menu-items');
+
+const nav = document.querySelectorAll('.menu-items li') 
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('close');
+    menuItems.classList.toggle('show');
+});
+
+nav.forEach( item => (e) => {
+    item.classList.remove('active');
+    console.log(e)
+    item.addEventListener('click', () => {
+        item.classList.add('active');
+        console.log('foi')
+    })
+})
+
+function highlight(event) {
+    const navItem = event.currentTarget;
+
+    nav.forEach(item => {
+        item.classList.remove('active')
+    });
+
+    navItem.classList.add('active');
+}
