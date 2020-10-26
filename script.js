6ca597c1759264ffcc513046dab05807209f9ae4
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2020-10-26 19:00:15 -0300

    resp issue

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ const menuItems = document.querySelector('.menu-items');
 
 const nav = document.querySelectorAll('.menu-items li') 
 
+// menu burguer 
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('close');
     menuItems.classList.toggle('show');
 });
 
-nav.forEach( item => (e) => {
-    item.classList.remove('active');
-    console.log(e)
-    item.addEventListener('click', () => {
-        item.classList.add('active');
-        console.log('foi')
-    })
-})
+// nav.forEach( item => (e) => {
+//     item.classList.remove('active');
+//     console.log(e)
+//     item.addEventListener('click', () => {
+//         item.classList.add('active');
+//         console.log('foi')
+//     })
+// })
 
 function highlight(event) {
     const navItem = event.currentTarget;
