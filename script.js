9a7473cde1c9cfd344c78e7b436a3df2922ce935
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2020-10-29 20:09:15 -0300

    Pages: About/Work

--- script.js
+++ script.js
@@ -9,21 +9,93 @@ menuIcon.addEventListener('click', () => {
     menuItems.classList.toggle('show');
 });
 
-// nav.forEach( item => (e) => {
-//     item.classList.remove('active');
-//     console.log(e)
-//     item.addEventListener('click', () => {
-//         item.classList.add('active');
-//         console.log('foi')
-//     })
-// })
+// ARROW ICON
 
-function highlight(event) {
-    const navItem = event.currentTarget;
+// photos
+const arrowPhotos = document.querySelector('#photos-down');
 
-    nav.forEach(item => {
-        item.classList.remove('active')
+arrowPhotos.addEventListener('click', () => {
+    arrowPhotos.classList.toggle('fa-angle-double-up');
+});
+
+// videos
+const arrowVideos = document.querySelector('#videos-down');
+
+arrowVideos.addEventListener('click', () => {
+    arrowVideos.classList.toggle('fa-angle-double-up')
+})
+
+// modal 
+const modal = document.querySelector('.modal');
+const photos = document.querySelectorAll('.photo');
+
+photos.forEach(photo => {
+    photo.addEventListener('click', openModal);
+})
+
+function openModal(event) {
+    const closeIcon = document.querySelector('#close');
+
+    // opening model
+    const photo = event.currentTarget;
+    modal.classList.add('active');
+
+    // getting image 
+    const imgContainer = document.querySelector('.img-container');
+    let img = photo.cloneNode(true);
+
+    // change photo - arrow forward
+    const arrowForward = document.querySelector('#arrow-modal-right');
+
+    // getting photo index
+    let index = Array.from(photos).indexOf(photo);
+
+    
+    arrowForward.addEventListener('click', () => {
+        if(index < photos.length - 1) {
+            // remove current photo from container
+            imgContainer.removeChild(img);
+            
+            // clone new photo div by array position
+            const nextPhoto = photos[index + 1].cloneNode(true);
+    
+            // imgContainer.remove(photo)
+            img = nextPhoto;
+            imgContainer.appendChild(img);
+    
+            index++
+        }
+    })
+
+    // console.log(oldIndex)
+
+    // change photo - arrow forward
+    const arrowBackward = document.querySelector('#arrow-modal-left');
+    
+    arrowBackward.addEventListener('click', () => {
+        if(index > 0) {
+            // remove current photo from container
+            imgContainer.removeChild(img);
+            
+            // clone new photo div by array position
+            const nextPhoto = photos[index - 1].cloneNode(true);
+    
+            // imgContainer.remove(photo)
+            img = nextPhoto;
+            imgContainer.appendChild(img);
+    
+            index--
+        }
+    })
+
+
+    // add img to imgContainer
+    imgContainer.appendChild(img);
+
+    // closing modal
+    closeIcon.addEventListener('click', () => {
+        modal.classList.remove('active');
+        imgContainer.removeChild(img);
     });
 
-    navItem.classList.add('active');
 }
